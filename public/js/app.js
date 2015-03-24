7e2717743aa5d59ae9136a19046d212370c10544
lorisfo <loris-fo@outlook.com>
2015-03-24 15:39:34 +0000

    Added mockup website

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,5 @@
 var legsorSausages = angular.module('LegsorSausages', ['ngResource']);
 
 legsorSausages.controller('legsOrSausagesController', function($scope) {
-  $scope.greeting = "heko";
+  $scope.greeting = "hello";
 });
