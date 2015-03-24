9247242506f14a5696243699388f3ce3ea174af3
lorisfo <loris-fo@outlook.com>
2015-03-24 16:40:02 +0000

    added test

--- test/legsOrSausagesController.spec.js
+++ test/legsOrSausagesController.spec.js
@@ -9,4 +9,14 @@ describe('legsOrSausagesController', function() {
         $scope: scope
     });
   }));
+
+  it('should have two images that a user can vote on', function(){
+    expect(scope.images.length).toEqual(2);
+  });
+
+  it('should show a new image after user has voted', function(){
+    // scope.vote();
+    expect(scope.images[1].name).toEqual("Photo2")
+  });
+
 });
