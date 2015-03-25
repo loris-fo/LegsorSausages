afe402b8bdd5edaf077931a644afe8b1639c0b83
lorisfo <loris-fo@outlook.com>
2015-03-25 13:16:49 +0000

    Extracted code

---
+++ app_jquery.js
@@ -0,0 +1,46 @@
+// $(document).ready(function(){
+
+// $("#legs_button").click(function(){
+//     $('#main_image').fadeToggle( 0, function() {
+//        $('#main_image').fadeIn( 2000, function() {
+
+//   });
+//   });
+// });
+
+// $("#sausages_button").click(function(){
+//     $('#main_image').fadeToggle( 0, function() {
+//        $('#main_image').fadeIn( 2000, function() {
+
+//   });
+//   });
+// });
+
+// });
+$(document).ready(function(){
+
+    $(".frame").on("swiperight",function(){
+      $(this).addClass('rotate-left').delay(700).fadeOut(1);
+      $('.frame').find('.status').remove();
+
+      $(this).append('<div class="status sausages">Sausages!</div>');
+      if ( $(this).is(':last-child') ) {
+        $('.frame:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+       } else {
+          $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+       }
+    });
+
+   $(".frame").on("swipeleft",function(){
+    $(this).addClass('rotate-right').delay(700).fadeOut(1);
+    $('.frame').find('.status').remove();
+    $(this).append('<div class="status legs">Legs!</div>');
+
+    if ( $(this).is(':last-child') ) {
+     $('.frame:nth-child(1)').removeClass ('rotate-left rotate-right').fadeIn(300);
+     } else {
+        $(this).next().removeClass('rotate-left rotate-right').fadeIn(400);
+    }
+  });
+
+});
