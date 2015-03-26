806ab373305181b2aed69424d88d06a8d7031553
lorisfo <loris-fo@outlook.com>
2015-03-26 11:26:08 +0000

    Heroku push

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var server = express();
 var path = require('path');
 
+server.set('port', (process.env.PORT || 5000))
+
 server.use(express.static(__dirname + '/public'));
 server.use('/bower_components', express.static(__dirname + '/bower_components'));
 
@@ -9,7 +11,6 @@ server.use('/bower_components', express.static(__dirname + '/bower_components'))
     response.sendFile(path.join(__dirname + '/views/index.html'));
   });
 
-  server.listen(3000, function(){
-    console.log('Listening on port 3000');
-  });
-
+  server.listen(server.get('port'), function() {
+  console.log("Node app is running at localhost:" + server.get('port'))
+})
