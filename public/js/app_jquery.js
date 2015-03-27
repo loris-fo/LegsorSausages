2cc4b9e3576d6be55b8957d3b1d35f4b581d6549
lorisfo <loris-fo@outlook.com>
2015-03-27 11:20:52 +0000

    Added tests

--- public/js/app_jquery.js
+++ public/js/app_jquery.js
@@ -1,41 +1,41 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
 
-var angle = 360;
+// var angle = 360;
 
-$('#sausage_vote').click(function(){
-  $('#main_photo').css({
-    '-webkit-transform': 'rotate(' + angle + 'deg)',
-    '-moz-transform': 'rotate(' + angle + 'deg)',
-    '-o-transform': 'rotate(' + angle + 'deg)',
-    '-ms-transform': 'rotate(' + angle + 'deg)'
+// $('#sausage_vote').click(function(){
+//   $('#main_photo').css({
+//     '-webkit-transform': 'rotate(' + angle + 'deg)',
+//     '-moz-transform': 'rotate(' + angle + 'deg)',
+//     '-o-transform': 'rotate(' + angle + 'deg)',
+//     '-ms-transform': 'rotate(' + angle + 'deg)'
 
-  });
-  angle+=360;
-})
+//   });
+//   angle+=360;
+// })
 
-$('#legs_vote').click(function(){
-  $('#main_photo').css({
-    '-webkit-transform': 'rotate(' + angle + 'deg)',
-    '-moz-transform': 'rotate(' + angle + 'deg)',
-    '-o-transform': 'rotate(' + angle + 'deg)',
-    '-ms-transform': 'rotate(' + angle + 'deg)'
+// $('#legs_vote').click(function(){
+//   $('#main_photo').css({
+//     '-webkit-transform': 'rotate(' + angle + 'deg)',
+//     '-moz-transform': 'rotate(' + angle + 'deg)',
+//     '-o-transform': 'rotate(' + angle + 'deg)',
+//     '-ms-transform': 'rotate(' + angle + 'deg)'
 
-  });
-  angle-=360;
-})
+//   });
+//   angle-=360;
+// })
 
-  $('#legs_vote').click(function(){
-    $( "#correctanswer" ).fadeIn()
-    $( "#correctanswer" ).fadeOut()
-    $( "#incorrectanswer" ).fadeIn()
-    $( "#incorrectanswer" ).fadeOut()
-  });
-    $('#sausage_vote').click(function(){
-    $( "#correctanswer" ).fadeIn()
-    $( "#correctanswer" ).fadeOut()
-    $( "#incorrectanswer" ).fadeIn()
-    $( "#incorrectanswer" ).fadeOut()
-  });
+//   $('#legs_vote').click(function(){
+//     $( "#correctanswer" ).fadeIn()
+//     $( "#correctanswer" ).fadeOut()
+//     $( "#incorrectanswer" ).fadeIn()
+//     $( "#incorrectanswer" ).fadeOut()
+//   });
+//     $('#sausage_vote').click(function(){
+//     $( "#correctanswer" ).fadeIn()
+//     $( "#correctanswer" ).fadeOut()
+//     $( "#incorrectanswer" ).fadeIn()
+//     $( "#incorrectanswer" ).fadeOut()
+//   });
 
-});
+// });
--- test/legsOrSausagesController.spec.js
+++ test/legsOrSausagesController.spec.js
@@ -1,36 +1,66 @@
 describe('legsOrSausagesController', function() {
   beforeEach(module('LegsorSausages'));
 
-  var scope, ctrl;
+  var httpBackend, scope, ctrl;
 
-  beforeEach(inject(function($rootScope, $controller) {
+  beforeEach(inject(function($rootScope, $controller, $httpBackend, $http) {
+    httpBackend = $httpBackend;
+    httpBackend.when("GET", "/images").respond([{id: 1, image: '/images/image1.jpg', sausage: false}, {id: 2, image: '/images/image2.jpg', sausage: true}]);
     scope = $rootScope.$new();
     ctrl = $controller('legsOrSausagesController', {
-        $scope: scope
+        $scope: scope,
+        $http: $http
     });
   }));
 
-  it('should have two images that a user can vote on', function(){
-    expect(scope.photos.length).toEqual(2);
+
+
+  describe('initialisation settings', function(){
+
+    it('each image should have a sausage key', function(){
+      httpBackend.flush();
+      expect(scope.photos[0].sausage).toEqual(false);
+    });
+
+    it('each image should have an image url', function(){
+      httpBackend.flush();
+      expect(scope.photos[0].image).toEqual('/images/image1.jpg');
+    });
+
+
+    it('each image should have an id', function(){
+      httpBackend.flush();
+      expect(scope.photos[0].id).toEqual(1);
+    });
+
+    it('should have two images that a user can vote on', function(){
+      httpBackend.flush();
+      expect(scope.photos.length).toEqual(2);
+    });
+
   });
 
-  it('should show a new image after user has voted', function(){
-    scope.changePhoto();
-    expect(scope.showPhoto()).toEqual(scope.photos[1].url);
-  });
 
-  it('each image should have a sausage key', function(){
-    expect(scope.photos[0].sausage).toEqual(false)
-  });
+  describe('game settings', function(){
 
-  it('should increase the score if the user chooses the correct answer', function(){
-    scope.legs();
-    expect(scope.score).toEqual(1);
-  });
+    it('should show a new image after user has voted', function(){
+      httpBackend.flush();
+      scope.changePhoto();
+      expect(scope.showPhoto()).toEqual(scope.photos[1].image);
+    });
 
-  it('should know if a user chooses the incorrect answer', function(){
-    scope.sausage();
-    expect(scope.score).toEqual(-1);
+    it('should increase the score if the user chooses the correct answer', function(){
+      httpBackend.flush();
+      scope.legs();
+      expect(scope.score).toEqual(1);
+    });
+
+    it('should decrease the score if the user chooses the incorrect answer', function(){
+      httpBackend.flush();
+      scope.sausage();
+      expect(scope.score).toEqual(-1);
+    });
+
   });
 
 });
