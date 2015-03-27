d117e4c9638d8db58b7224a1d5ff9853bcc40536
lorisfo <loris-fo@outlook.com>
2015-03-27 11:29:17 +0000

    Update app_jquery.js

--- app_jquery.js
+++ app_jquery.js
@@ -1,22 +1,3 @@
-// $(document).ready(function(){
-
-// $("#legs_button").click(function(){
-//     $('#main_image').fadeToggle( 0, function() {
-//        $('#main_image').fadeIn( 2000, function() {
-
-//   });
-//   });
-// });
-
-// $("#sausages_button").click(function(){
-//     $('#main_image').fadeToggle( 0, function() {
-//        $('#main_image').fadeIn( 2000, function() {
-
-//   });
-//   });
-// });
-
-// });
 $(document).ready(function(){
 
     $(".frame").on("swiperight",function(){
